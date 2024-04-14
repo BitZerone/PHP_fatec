0726d6b5f67b35cbafced81d639f01bacf778452
BitZerone <138386422+BitZerone@users.noreply.github.com>
2024-04-14 16:17:12 -0300

    Add files via upload

---
+++ trabalhoRotas/public/exer10.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+<body>
+    <h3>Preencha os campos: </h3>
+    <form action="/exer10/resposta" method="post">
+        <input type="text" name="altura"
+        placeholder="Digite sua altura">
+        <input type="text" name="peso"
+        placeholder="Digite seu peso">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ trabalhoRotas/public/exer9.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <h3>Digite sua data de nascimento: </h3>
+    <form action="/exer9/resposta" method="post">
+        <input type="date" name="dtnasc">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
